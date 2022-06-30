32e84936ccdced9f46d44b87c7f3ef52e75d1895
Eugine <villareugine26@gmail.com>
2022-06-30 22:24:52 +0800

    added login page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './main/main.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    LoginPageComponent
   ],
   imports: [
     BrowserModule
