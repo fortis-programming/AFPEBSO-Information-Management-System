9f7e1f483dc1c73e97837538a694b3d5d8390737
Allain <itsallain@gmail.com>
2022-06-30 23:34:35 +0800

    added main routing

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'app',
+    pathMatch: 'full',
+  },
+  {
+    path: 'app',
+    loadChildren: () => import('./main/main.module').then((m) => m.MainModule),
+  },
+];
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, RouterModule.forRoot(routes)],
+})
+export class AppRoutingModule {}
---
+++ src/app/home-page/home-page-route.module.ts
@@ -0,0 +1,9 @@
+import { Routes } from "@angular/router";
+import { HomePageComponent } from "./home-page.component";
+
+export const route: Routes = [
+  {
+    path: 'home',
+    component: HomePageComponent
+  }
+];
