69ecf87debebf3ca755ec7230a4bd501240c3c04
Allain <itsallain@gmail.com>
2022-07-01 20:28:44 +0800

    fix login ui

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'app',
+    redirectTo: 'login',
     pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    component: LoginPageComponent
   },
   {
     path: 'app',
