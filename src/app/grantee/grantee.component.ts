6f308dd7bd69c3abefd753a56c3e1fd709290744
Eugine <villareugine26@gmail.com>
2022-07-05 22:52:09 +0800

    added grantee model

--- src/app/grantee/grantee.component.ts
+++ src/app/grantee/grantee.component.ts
@@ -5,13 +5,74 @@ import { GranteeModel } from '../_shared/models/grantee.model';
 @Component({
   selector: 'app-grantee',
   templateUrl: './grantee.component.html',
-  styleUrls: ['./grantee.component.scss']
+  styleUrls: ['./grantee.component.scss'],
 })
 export class GranteeComponent implements OnInit {
+  granteeModel: GranteeModel = {
+    date: '',
+    id: '',
+    area: '',
+    surname: '',
+    firstname: '',
+    middlename: '',
+    relationshipToAfpMember: '',
+    currentAddress: '',
+    provincialAddress: '',
+    phoneNumber: '',
+    cellPhoneNumber: '',
+    emailAddress: '',
+    birthDate: '',
+    birthPlace: '',
+    sex: '',
+    civilStatus: '',
+    religion: '',
+    schoolIntendedToEnrollIn: '',
+    schoolAddress: '',
+    educationLevel: '',
+    yearLevel: '',
+    course: '',
 
-  constructor() { }
+    //information of AFP/CAA member
+    afp_surname: '',
+    afp_firstName: '',
+    afp_middleInitial: '',
+    afp_rank: '',
+    afp_SN: '',
+    afp_branchOfService: '',
+    afp_militaryStatus: '',
+    afp_unitAssignment: '',
+    afp_currentAddress: '',
+    afp_phoneNo: '',
+    afp_cellPhoneNo: '',
+    afp_emailAddress: '',
+    afp_birthDate: '',
+    afp_birthPlace: '',
+    afp_sex: '',
+    afp_civilStatus: '',
+    afp_nrOfDependents: '',
+    afp_nameOfLegalDependents: '',
+    afp_dependentsBirthdate: '',
+    afp_dependentsYearLevel: '',
+    afp_educationalProgramAvailed: '',
 
-  ngOnInit(): void {
-  }
+    //information of Applicant's guardian
+    guardian_surname: '',
+    guardian_firstName: '',
+    guardian_MiddleName: '',
+    guardian_relationshipToApplicant: '',
+    guardian_currentAddress: '',
+    guardian_provincialAddress: '',
+    guardian_emailAddress: '',
+    guardian_cellPhoneNo: '',
+    guardian_phoneNo: '',
 
+    signatureOfApplicant: '',
+    signatureOfAfporGuardian: '',
+
+    nameOfAfpPersonnel: '',
+    dateReceived: '',
+  };
+  constructor() {}
+
+  ngOnInit(): void {}
 }
