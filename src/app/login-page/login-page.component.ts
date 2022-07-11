e161eb050200e98a9774cd34c7ff5b599b47433d
Allain <itsallain@gmail.com>
2022-07-11 10:27:45 +0800

    added login authentication

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { GraduatesModels } from '../_shared/models/graduates.models';
-import { GranteeModel } from '../_shared/models/grantee.model';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../services/authentication.service';
 import { LoginRequest } from '../_shared/models/requests/login.requests';
 
 @Component({
@@ -14,11 +14,28 @@ export class LoginPageComponent implements OnInit {
     password: ''
   }
 
-  constructor() { }
+  constructor(
+    private authenticationService: AuthenticationService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
   }
 
+  processing = false;
+  showMessage = false;
+  loginUser(): void {
+    this.processing = true;
+    this.authenticationService.signInUser(this.loginRequest).then((response) => {
+      console.log(response);
+      (response === "auth/invalid-email" || response === "auth/wrong-password")? this.showMessage = true :  this.router.navigateByUrl('app');
+      this.processing = false;
+    }).catch((error) => {
+      console.log(error);
+      this.processing = false;
+    });
+  }
+  
   hasError(formControl: any): boolean {
     return formControl.invalid && (formControl.dirty || formControl.touched)
   }
