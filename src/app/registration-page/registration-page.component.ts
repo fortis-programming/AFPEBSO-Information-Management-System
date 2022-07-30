8d15893bd3a17cd9848651d852e176efd63c88ee
Eugine <villareugine26@gmail.com>
2022-07-30 23:07:12 +0800

    registration page binded

--- src/app/registration-page/registration-page.component.ts
+++ src/app/registration-page/registration-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GranteeModel } from '../_shared/models/grantee.model';
 
 @Component({
   selector: 'app-registration-page',
@@ -6,6 +7,75 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registration-page.component.scss']
 })
 export class RegistrationPageComponent implements OnInit {
+  step = 0;
+  registration: GranteeModel = {
+    dateSubmitted: '',
+    status: '',
+    profileUrl: '',
+    date: '',
+    id: '',
+    area: '',
+    surname: '',
+    firstname: '',
+    middlename: '',
+    relationship_to_afp_member: '',
+    currentAddress: '',
+    provincialAddress: '',
+    phoneNumber: '',
+    cellphoneNumber: '',
+    emailAddress: '',
+    birthDate: '',
+    birthPlace: '',
+    sex: '',
+    civilStatus: '',
+    religion: '',
+    schoolIntendedToEnrollIn: '',
+    schoolAddress: '',
+    educationLevel: '',
+    yearLevel: '',
+    course: '',
+
+    //information of AFP/CAA member
+    afp_surname: '',
+    afp_firstname: '',
+    afp_middleInitial: '',
+    afp_rank: '',
+    afp_SN: '',
+    afp_branchOfService: '',
+    afp_militaryStatus: '',
+    afp_unitAssignment: '',
+    afp_currentAddress: '',
+    afp_phoneNo: '',
+    afp_cellPhoneNo: '',
+    afp_emailAddress: '',
+    afp_birthDate: '',
+    afp_birthPlace: '',
+    afp_sex: '',
+    afp_civilStatus: '',
+    afp_nrOfDependents: '',
+    afp_nameOfLegalDependents: '',
+    afp_dependentsBirthdate: '',
+    afp_dependentsYearLevel: '',
+    afp_educationalProgramAvailed: '',
+    afp_date_of_death: '',
+
+    //information of Applicant's guardian
+    guardian_surname: '',
+    guardian_firstName: '',
+    guardian_MiddleName: '',
+    guardian_relationshipToApplicant: '',
+    guardian_currentAddress: '',
+    guardian_provincialAddress: '',
+    guardian_emailAddress: '',
+    guardian_cellPhoneNo: '',
+    guardian_phoneNo: '',
+
+    signatureOfApplicant: '',
+    signatureOfAfporGuardian: '',
+
+    nameOfAfpPersonnel: '',
+    dateReceived: '',
+  };
 
   constructor() { }
 
