dc3ab067c99cf726ba3fd05335edfcae8a966bc8
Eugine E. Villar <eugine@sdsolutions.com.ph>
2022-09-02 00:15:29 +0800

    added remarks component

--- src/app/remarks/remarks.component.ts
+++ src/app/remarks/remarks.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-remarks',
@@ -6,8 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./remarks.component.scss']
 })
 export class RemarksComponent implements OnInit {
+  @Input() remarksMessage = '';
 
   constructor() { }
+
+  status = 'Declined';
 
   ngOnInit(): void {
   }
